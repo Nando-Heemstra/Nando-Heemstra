b37a5789ddd88dba40877176d85e86fdcb987054
Nando Heemstra <nandoheemstra@gmail.com>
2026-01-07 00:00:19 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -19,3 +19,24 @@ document.querySelectorAll(".faq-question").forEach(button => {
         answer.classList.toggle("active");
     });
 });
+
+function toggleDescription(skillElement) {
+    const desc = skillElement.querySelector(".skill-description");
+
+    // Toggle logic
+    if (desc.style.maxHeight && desc.style.maxHeight !== "0px") {
+        desc.style.maxHeight = "0px";
+        desc.style.marginTop = "0px";
+    } else {
+        // Set to scrollHeight to allow dynamic expansion
+        desc.style.maxHeight = desc.scrollHeight + "px";
+        desc.style.marginTop = "10px";
+    }
+}
+
+// Ensure entry animations for the rest of the site work
+window.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll('.slide-left, .slide-right').forEach(el => {
+        el.classList.add('show');
+    });
+});
